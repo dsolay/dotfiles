d4543058f9e406336f2fafb8c9052dd3ed1a20cf
Protesilaos Stavrou <info@protesilaos.com>
2019-04-24 13:34:05 +0300

    Beta 1 of "Prot's Dots For Debian (PDFD)"
    The following is copied from the commit message of `v1.0.0-pdfd_beta1`
    from my dotfiles' repo: https://gitlab.com/protesilaos/dotfiles
    
        PDFD is the book that guides users through the process of setting up
        my custom desktop session.  It also provides information on the
        various aspects of my dotfiles.
    
        PDFD is meant to use the latest "fixed release" of my dotfiles,
        denoted by tags such as this one.  The repo's contents are copied
        into a separate repository from where PDFD reads from.  That repo is
        called "Code for PDFD (CPDFD)": https://gitlab.com/protesilaos/cpdfd
    
        PDFD only reads from CPDFD, which in turn only contains tagged
        releases of my dotfiles.

---
+++ colours/.local/share/my_colours/shell/tempus_night.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Can be sourced from scripts to expose these colours as variables.
+
+# theme: Tempus Night
+# author: Protesilaos Stavrou (https://protesilaos.com)
+# description: High contrast dark theme with bright colours (WCAG AAA compliant)
+background="#1a1a1a"
+foreground="#e0e0e0"
+backgroundalt="#18143d"
+foregroundalt="#c4bdaf"
+cursorColor="#e0e0e0"
+cursorColor2="#1a1a1a"
+color0="#1a1a1a"
+color1="#fb7e8e"
+color2="#52ba40"
+color3="#b0a800"
+color4="#5aaaf2"
+color5="#ee80c0"
+color6="#1db5c3"
+color7="#c4bdaf"
+color8="#18143d"
+color9="#f69d6a"
+color10="#88c400"
+color11="#d7ae00"
+color12="#8cb4f0"
+color13="#de99f0"
+color14="#00ca9a"
+color15="#e0e0e0"
+
+# Special colours
+backgrounddim="#19172b"
