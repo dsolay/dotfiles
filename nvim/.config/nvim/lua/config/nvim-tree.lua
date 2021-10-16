47cc6445dbb503d96ab61ec41c5e017700675034
Roman Gonzalez <solay2516@gmail.com>
2021-10-16 11:58:45 -0600

    feat(nvim, nvim-tree): update config

--- nvim/.config/nvim/lua/config/nvim-tree.lua
+++ nvim/.config/nvim/lua/config/nvim-tree.lua
@@ -1,7 +1,7 @@
 require('nvim-tree').setup {
-  disable_netrw   = false,
-  hijack_netrw    = false,
+  disable_netrw   = true,
+  hijack_netrw    = true,
   auto_close      = true,
-  lsp_diagnostics = true,
+  diagnostics = {enable = true},
   ignore_ft_on_setup  = { 'startify', 'dashboard' },
 }
