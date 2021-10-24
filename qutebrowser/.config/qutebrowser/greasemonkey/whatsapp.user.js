5d53a66e75621a39e1eb3397f115af9d0c7d5dfe
Roman Gonzalez <solay2516@gmail.com>
2021-10-23 19:25:18 -0600

    remove unused configs

--- qutebrowser/.config/qutebrowser/greasemonkey/whatsapp.user.js
+++
@@ -1,5 +0,0 @@
-// ==UserScript==
-// @include https://web.whatsapp.com/
-// ==/UserScript==
-if (document.body.innerText.replace(/\n/g, ' ').search(/whatsapp works with.*to use whatsapp.*update/i) !== -1)
-  navigator.serviceWorker.getRegistration().then(function (r) { r.unregister(); document.location.reload() });
