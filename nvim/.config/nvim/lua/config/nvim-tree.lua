a3f4a874149fad2eb1e09725682bd2812f574f4c
Roman Gonzalez <roman.gonzalez.ea@gmail.com>
2021-11-28 12:01:34 -0600

    show ignored files in nvim-tree

--- nvim/.config/nvim/lua/config/nvim-tree.lua
+++ nvim/.config/nvim/lua/config/nvim-tree.lua
@@ -1,7 +1,9 @@
 require('nvim-tree').setup {
-  disable_netrw   = true,
-  hijack_netrw    = true,
-  auto_close      = true,
-  diagnostics = {enable = true},
-  ignore_ft_on_setup  = { 'startify', 'dashboard' },
+    disable_netrw = true,
+    hijack_netrw = true,
+    auto_close = true,
+    diagnostics = {enable = true},
+    ignore_ft_on_setup = {'startify', 'dashboard'},
+    git = {enable = true, ignore = false, timeout = 500},
 }
+
