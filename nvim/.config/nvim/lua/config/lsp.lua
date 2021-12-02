fde6c1ccba9e20a037ad5032a50ee81e80ccd611
Roman Gonzalez <roman.gonzalez.ea@gmail.com>
2021-12-02 09:24:04 -0600

    fix(nvim_lsp): correct sign icons

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -1,6 +1,6 @@
 local merge = vim.tbl_extend
 
-local signs = { Error = "✘", Warn = "", Hint = "", Info = "" }
+local signs = {Error = '✘', Warn = '', Info = '', Hint = ''}
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, linehl = '', numhl = '' })
