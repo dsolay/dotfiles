531c7f3eec23f08485ccd348cad0797d51bf97a0
Roman Gonzalez <roman.gonzalez.ea@gmail.com>
2022-01-06 15:46:06 -0600

    remove js and ts from lsp stylelint filetypes

--- nvim/.config/nvim/lua/servers/stylelint.lua
+++ nvim/.config/nvim/lua/servers/stylelint.lua
@@ -1,10 +1,21 @@
 return {
+    filetypes = {
+        'css',
+        'less',
+        'scss',
+        'sugarss',
+        'vue',
+        'wxss',
+        'javascriptreact',
+        'typescriptreact',
+    },
     settings = {
         stylelintplus = {
             autoFixOnFormat = false,
             autoFixOnSave = false,
             validateOnSave = true,
             validateOnType = false,
-        }
+        },
     },
 }
+
