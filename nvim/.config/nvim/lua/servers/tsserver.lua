a1fd5771228f32de87cfa25546766a358841e5f7
Roman Gonzalez <roman.gonzalez.ea@gmail.com>
2022-01-25 09:07:03 -0600

    feat(nvim, lsp): add nvim-lsp-ts-plugin

--- nvim/.config/nvim/lua/servers/tsserver.lua
+++ nvim/.config/nvim/lua/servers/tsserver.lua
@@ -1,6 +1,26 @@
 return {
-    on_attach = function(client)
+    init_options = require('nvim-lsp-ts-utils').init_options,
+    on_attach = function(client, bufnr)
+        local ts_utils = require('nvim-lsp-ts-utils')
+
         client.resolved_capabilities.document_formatting = false
         client.resolved_capabilities.document_range_formatting = false
+
+        ts_utils.setup({filter_out_diagnostics_by_code = {80001}})
+
+        -- required to fix code action ranges and filter diagnostics
+        ts_utils.setup_client(client)
+
+        -- no default maps, so you may want to define some here
+        local opts = {silent = true}
+        vim.api.nvim_buf_set_keymap(
+            bufnr, 'n', '<leader>oi', ':TSLspOrganize<CR>', opts
+        )
+        vim.api.nvim_buf_set_keymap(
+            bufnr, 'n', '<leader>trn', ':TSLspRenameFile<CR>', opts
+        )
+        vim.api.nvim_buf_set_keymap(
+            bufnr, 'n', '<leader>ia', ':TSLspImportAll<CR>', opts
+        )
     end,
 }
