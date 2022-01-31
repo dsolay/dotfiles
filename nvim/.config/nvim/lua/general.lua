755ba088e4ef1a937ca4b094655a9edc2595c786
Roman Gonzalez <roman.gonzalez.ea@gmail.com>
2022-01-31 11:25:42 -0600

    fix: wrong python path in nvim

--- nvim/.config/nvim/lua/general.lua
+++ nvim/.config/nvim/lua/general.lua
@@ -12,7 +12,7 @@ g.loaded_ruby_provider = 0
 g.loaded_perl_provider = 0
 
 g.python_host_prog = '/usr/bin/python2'
-g.python3_host_prog = '/usr/bin/python'
+g.python3_host_prog = '/usr/bin/python3'
 
 -- Disable some built-in plugins we don't want
 local disabled_built_ins = {
