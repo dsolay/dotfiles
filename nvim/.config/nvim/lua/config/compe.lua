4e237a791a1cf26f226debadd6859245ba08d096
Roman Gonzalez <roman.gonzalez.ea@gmail.com>
2022-02-05 12:33:36 -0600

    nvim: update lspkind config

--- nvim/.config/nvim/lua/config/compe.lua
+++ nvim/.config/nvim/lua/config/compe.lua
@@ -12,12 +12,21 @@ local source_mapping = {
 cmp.setup(
     {
         formatting = {
-            format = function(entry, vim_item)
-                vim_item.kind = lspkind.presets.default[vim_item.kind]
-                local menu = source_mapping[entry.source.name]
-                vim_item.menu = menu
-                return vim_item
-            end,
+            format = lspkind.cmp_format(
+                {
+                    mode = 'symbol', -- show only symbol annotations
+                    maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+
+                    -- The function below will be called before any actual modifications from lspkind
+                    -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+                    before = function(entry, vim_item)
+                        vim_item.kind = lspkind.presets.default[vim_item.kind]
+                        local menu = source_mapping[entry.source.name]
+                        vim_item.menu = menu
+                        return vim_item
+                    end,
+                }
+            ),
         },
         snippet = {
             expand = function(args)
