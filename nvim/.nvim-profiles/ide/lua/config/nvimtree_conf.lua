2da10377adcaa7b0e7748b6d6138605e821322fe
Roman Gonzalez <roman.gonzalez.ea@gmail.com>
2022-03-27 18:18:59 -0600

    refactor(nvim, tree): remove auto_close option

--- nvim/.nvim-profiles/ide/lua/config/nvimtree_conf.lua
+++ nvim/.nvim-profiles/ide/lua/config/nvimtree_conf.lua
@@ -1,7 +1,6 @@
 require('nvim-tree').setup {
     disable_netrw = true,
     hijack_netrw = true,
-    auto_close = true,
     diagnostics = {enable = true},
     ignore_ft_on_setup = {'startify', 'dashboard'},
     auto_reload_on_write = false,
