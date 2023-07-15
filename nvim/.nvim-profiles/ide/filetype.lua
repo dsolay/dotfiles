c96ec30e3c9e1fead127950b544c7626ee40241d
Roman Gonzalez <rgonzalez@betacode.dev>
2023-07-15 16:58:59 -0600

    feat(nvim, filetype): add dockerignore

--- nvim/.nvim-profiles/ide/filetype.lua
+++ nvim/.nvim-profiles/ide/filetype.lua
@@ -7,6 +7,7 @@ vim.filetype.add({
     filename = {
         [".env"] = "env",
         ["Dockerfile"] = "dockerfile",
+        [".dockerignore"] = "dockerignore",
     },
     pattern = {
         [".env.*"] = "env",
