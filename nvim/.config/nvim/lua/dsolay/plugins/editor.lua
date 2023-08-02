a3d245fe29a6e17857a971f0e7ced820565f2511
Roman Gonzalez <rgonzalez@betacode.dev>
2023-08-01 20:21:01 -0600

    feat(nvim): lazy load folke/todo-comments.nvim

--- nvim/.config/nvim/lua/dsolay/plugins/editor.lua
+++ nvim/.config/nvim/lua/dsolay/plugins/editor.lua
@@ -140,7 +140,6 @@ return {
     {
         "folke/todo-comments.nvim",
         cmd = { "TodoTrouble", "TodoTelescope" },
-        event = { "BufReadPost", "BufNewFile" },
         config = true,
         keys = {
             {
