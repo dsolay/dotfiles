bd57d4f5bb13b960e01b3b58d485c99f69bfff74
Roman Gonzalez <rgonzalez@betacode.dev>
2023-12-01 15:14:32 -0600

    feat(nvim): add simple nvim config

---
+++ nvim/.config/nvim-simple/init.lua
@@ -0,0 +1,15 @@
+-- Deshabilitar resaltado de sintaxis
+-- vim.cmd('syntax off')
+vim.o.syntax = "off"
+
+-- Desactivar el ajuste automático del ancho de línea
+vim.opt.wrap = false
+
+-- Desactivar el resaltado de búsqueda
+vim.opt.hlsearch = false
+
+-- Aumentar el tamaño del archivo que puede abrir en memoria
+-- vim.opt.maxmem = 5000 * 1024 * 1024 -- 5000MB en bytes
+
+-- Ajustar el tamaño máximo del archivo a abrir
+-- vim.opt.maxmempattern = 5000 * 1024 * 1024 -- 5000MB en bytes
