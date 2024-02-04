3c42d648138f1428af0f127c12eb95cb516ff71e
Roman Gonzalez <roman.gonzalez@chivowallet.com>
2024-02-03 18:31:48 -0600

    fix(nvim, snipes): snip not work in tsx or jsx files

--- nvim/.config/nvim/lua/dsolay/plugins/coding.lua
+++ nvim/.config/nvim/lua/dsolay/plugins/coding.lua
@@ -140,6 +140,11 @@ return {
         dependencies = {
             "rafamadriz/friendly-snippets",
             config = function()
+                local luasnip = require("luasnip")
+
+                luasnip.filetype_extend("javascriptreact", { "html" })
+                luasnip.filetype_extend("typescriptreact", { "html" })
+
                 require("luasnip.loaders.from_vscode").lazy_load()
             end,
         },
