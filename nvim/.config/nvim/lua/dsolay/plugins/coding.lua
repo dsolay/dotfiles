e36dbf3b4a921fb6ee46d55929cefdba08465ec5
Roman Gonzalez <rgonzalez@betacode.dev>
2024-11-26 08:21:06 -0600

    fix(nvim, cpm): mappings not work for cmdline

--- nvim/.config/nvim/lua/dsolay/plugins/coding.lua
+++ nvim/.config/nvim/lua/dsolay/plugins/coding.lua
@@ -82,11 +82,24 @@ return {
                 }, { { name = "buffer" } }),
             })
 
-            -- Use buffer source for `/`.
-            cmp.setup.cmdline("/", { sources = { { name = "buffer" } } })
+            -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+            cmp.setup.cmdline({ "/", "?" }, {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = "buffer" },
+                },
+            })
 
-            -- Use cmdline & path source for ':'.
-            cmp.setup.cmdline(":", { sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }) })
+            -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+            cmp.setup.cmdline(":", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = "path" },
+                }, {
+                    { name = "cmdline" },
+                }),
+                matching = { disallow_symbol_nonprefix_matching = false },
+            })
         end,
         dependencies = {
             "hrsh7th/cmp-nvim-lsp",
