1e9d2e74c7ef85a9bbee4e8e827030133ec670e5
Roman Gonzalez <rgonzalez@betacode.dev>
2025-01-25 15:41:05 -0600

    feat(nvim): disable supermaven

--- nvim/.config/nvim/lua/dsolay/plugins/coding.lua
+++ nvim/.config/nvim/lua/dsolay/plugins/coding.lua
@@ -328,6 +328,7 @@ return {
 
     {
         "supermaven-inc/supermaven-nvim",
+        enabled = false,
         config = function()
             require("supermaven-nvim").setup({})
         end,
