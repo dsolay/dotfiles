22282ac774588d0091602663336777871697c2de
Roman Gonzalez <roman.gonzalez@chivowallet.com>
2025-01-25 19:04:25 -0600

    feat(nvim): add mini.move and octo plugin

--- nvim/.config/nvim/lua/dsolay/plugins/editor.lua
+++ nvim/.config/nvim/lua/dsolay/plugins/editor.lua
@@ -574,4 +574,24 @@ return {
             },
         },
     },
+
+    {
+        "echasnovski/mini.move",
+        version = "*",
+        opts = {
+            options = {
+                reindent_linewise = true,
+            },
+        },
+    },
+
+    {
+        "pwntester/octo.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim",
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = true,
+    },
 }
