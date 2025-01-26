072693e258eb76e0cdd40059f68d239ee2130573
Roman Gonzalez <roman.gonzalez@chivowallet.com>
2025-01-25 19:57:26 -0600

    feat(nvim): install hardtime

--- nvim/.config/nvim/lua/dsolay/plugins/editor.lua
+++ nvim/.config/nvim/lua/dsolay/plugins/editor.lua
@@ -565,4 +565,11 @@ return {
         },
         config = true,
     },
+
+    -- lazy.nvim
+    {
+        "m4xshen/hardtime.nvim",
+        dependencies = { "MunifTanjim/nui.nvim" },
+        opts = {},
+    },
 }
