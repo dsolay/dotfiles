3c696cd11deacb3c0e202ff6e1ebb0ec4a237d36
Roman Gonzalez <rgonzalez@betacode.dev>
2025-06-01 13:59:09 -0600

    feat(nvim, avante): update config

--- nvim/.config/nvim/lua/dsolay/plugins/coding.lua
+++ nvim/.config/nvim/lua/dsolay/plugins/coding.lua
@@ -164,9 +164,11 @@ return {
             vendors = {
                 deepseek = {
                     __inherited_from = "openai",
-                    api_key_name = { "pass", "show", "deepseek.com/api-keys/roman-pc" },
+                    api_key_name = "DEEPSEEK_API_KEY",
                     endpoint = "https://api.deepseek.com",
                     model = "deepseek-chat",
+                    temperature = 0,
+                    max_tokens = 8192,
                 },
             },
             behaviour = {
