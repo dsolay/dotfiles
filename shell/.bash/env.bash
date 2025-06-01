c328e9c1dbad57476ed59751ad2c29dc1bced073
Roman Gonzalez <rgonzalez@betacode.dev>
2025-06-01 13:59:28 -0600

    feat(shell): add distillery to path

--- shell/.bash/env.bash
+++ shell/.bash/env.bash
@@ -7,6 +7,8 @@ prependpath "$HOME/.cargo/bin"
 
 prependpath "$HOME/go/bin"
 
+prependpath "$HOME/.distillery/bin"
+
 # Include my scripts in the PATH.  To avoid conflicts, I always prepend
 # `own_script_` to my files.  There are some exceptions though, where I
 # am confident that no conflicts will arrise.  See the 'bin' directory
