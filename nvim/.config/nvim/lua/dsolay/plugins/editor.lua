6809a1a4484503865f946072d61ff3868ca72d00
Roman Gonzalez <rgonzalez@betacode.dev>
2026-03-08 09:55:40 -0600

    chore(nvim, dadbod-ui): move config to init

--- nvim/.config/nvim/lua/dsolay/plugins/editor.lua
+++ nvim/.config/nvim/lua/dsolay/plugins/editor.lua
@@ -57,11 +57,9 @@ return {
             "DBUIFindBuffer",
         },
         init = function()
-            -- Your DBUI configuration
-            vim.g.db_ui_use_nerd_fonts = 1
-        end,
-        config = function()
             local helpers = require("dsolay.config.dadbod-ui")
+
+            -- Your DBUI configuration
             vim.g.db_ui_table_helpers = helpers
 
             vim.g.db_ui_auto_execute_table_helpers = 1
